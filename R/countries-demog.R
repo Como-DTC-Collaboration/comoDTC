006f9d961cd67ddb5d270c752d64ddcd3a1ce133
ben18785 <ben.c.lambert@gmail.com>
2021-02-26 00:35:32 +0000

    updated dummy docs

--- R/countries-demog.R
+++ R/countries-demog.R
@@ -4,10 +4,5 @@
 #' diamonds.
 #'
 #' @format A data frame with 53940 rows and 10 variables:
-#' \describe{
-#'   \item{price}{price, in US dollars}
-#'   \item{carat}{weight of the diamond, in carats}
-#'   ...
-#' }
 #' @source \url{http://www.diamondse.info/}
 "countries_demog"
