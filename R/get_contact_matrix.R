cc068dc53679b0148028be90bbc5a9b1276944c7
Sumali Bajaj <sumali.bajaj@gmail.com>
2021-03-10 16:39:57 +0000

    Edited, added function documentation
    Added "5yr" in the input for number of age groups, loading contact home data in the test.

--- R/get_contact_matrix.R
+++ R/get_contact_matrix.R
@@ -1,21 +1,24 @@
-# function that loads and processes a contact matrix
-# for use in the como model ode solver
-get_contact_matrix <- function(country, contact_type, n_agegroups_popstruc){
-  #inputs:
-  # coutry: string representing the country for which the contact matrix will
-  # be loaded. Naming conventions for countries must be in line with those used
-  # in input excel file
+#' Create a contact matrix
+#'
+#' Function that loads and processes a contact matrix for use in the CoMo model ode solver
+#'
+#' @param country String representing the country for which the contact matrix will
+#' be loaded. Naming conventions for countries must be in line with those used
+#' in input excel file.
+#' @param contact_type A loaded matrix representing the contact between different age groups
+#' in a given social setting e.g. contact_work gives contact matrix for the work place.
+#' @param n_5yr_agegroups_popstruc Number of 5 year age groups starting with 0-4yr in the population.
+#' @return A square contact matrix for specified country with dimension equal to \code{n_5yr_agegroups_popstruc}
+#' @examples
+#' get_contact_matrix("United Kingdom of Great Britain", contact_home, 21)
+#'
+get_contact_matrix <- function(country, contact_type, n_5yr_agegroups_popstruc){
 
-  #contact_type: a loaded matrix representing the contact between different age groups
-  # in a given social setting e.g. contact_work gives contact matrix for the work
-  #place
-
-  #
   c <- contact_type[[country]] %>% as.matrix()
-  contact <- matrix(0, nrow = n_agegroups_popstruc, ncol = n_agegroups_popstruc)
+  contact <- matrix(0, nrow = n_5yr_agegroups_popstruc, ncol = n_5yr_agegroups_popstruc)
 
-  for (i in 1:n_agegroups_popstruc){
-    for(j in 1:n_agegroups_popstruc){
+  for (i in 1:n_5yr_agegroups_popstruc){
+    for(j in 1:n_5yr_agegroups_popstruc){
       contact[i,j] <- ifelse(i<=nrow(c) & j<=nrow(c), c[i,j],
                              ifelse(i<=nrow(c) & j>nrow(c), contact[i,nrow(c)], contact[nrow(c),j]))
     }
--- tests/testthat/test-get_contact_matrix.R
+++ tests/testthat/test-get_contact_matrix.R
@@ -1,10 +1,11 @@
 #unit testing for the get_contact_matrix.R function
-test_that("get_contact_matrix produces expected 21x21 output", {
+test_that("get_contact_matrix produces expected square matrix with number of specified 5 yr age groups' output", {
 
+  load("data/contact_home.rda")
   #run the function with Great Britain and contact_home
   home_matrix <- get_contact_matrix("United Kingdom of Great Britain", contact_home, 21)
 
-  #expect the length of both dimensions of the output to be 21
+  #expect the length of both dimensions of the output to be equal to many 5yr age groups are supplied
   expect_length(home_matrix[1,], 21)
   expect_length(home_matrix[,1], 21)
 
