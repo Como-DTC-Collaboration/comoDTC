f163e9ea4c395662796fdc87128f32fdb9c78687
Oscar Dimdore-Miles <32623897+oscardm20994@users.noreply.github.com>
2021-03-10 09:13:14 +0000

    creation of get_contact_matrix
    added function get_contact_matrix usins use_this()
    
    added testing for this function through test_that(()
    
    Co-Authored-By: Sumali Bajaj <34308572+sumalibajaj@users.noreply.github.com>

---
+++ R/get_contact_matrix.R
@@ -0,0 +1,26 @@
+# function that loads and processes a contact matrix
+# for use in the como model ode solver
+get_contact_matrix <- function(country, contact_type, n_agegroups_popstruc){
+  #inputs:
+  # coutry: string representing the country for which the contact matrix will
+  # be loaded. Naming conventions for countries must be in line with those used
+  # in input excel file
+
+  #contact_type: string representing the type of contact represented in the
+  #matrix to be generated. e.g. contact_work gives contact matrix for the work
+  #place
+
+  #
+  c <- contact_type[[country]] %>% as.matrix()
+  contact <- matrix(0, nrow = n_agegroups_popstruc, ncol = n_agegroups_popstruc)
+
+  for (i in 1:n_agegroups_popstruc){
+    for(j in 1:n_agegroups_popstruc){
+      contact[i,j] <- ifelse(i<=nrow(c) & j<=nrow(c), c[i,j],
+                             ifelse(i<=nrow(c) & j>nrow(c), contact[i,nrow(c)], contact[nrow(c),j]))
+    }
+  }
+
+  return(contact)
+}
+
