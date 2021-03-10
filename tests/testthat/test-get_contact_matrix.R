e9ea9584bca917a07b91c9f790e843ca3ee377eb
Oscar Dimdore-Miles <32623897+oscardm20994@users.noreply.github.com>
2021-03-10 18:31:28 +0000

    adapted testing
    Co-Authored-By: Sumali Bajaj <34308572+sumalibajaj@users.noreply.github.com>

--- tests/testthat/test-get_contact_matrix.R
+++ tests/testthat/test-get_contact_matrix.R
@@ -1,10 +1,10 @@
 #testing for the get_contact_matrix.R function
 test_that("get_contact_matrix produces expected 21x21 square matrix output", {
 
-  load("data/contact_home.rda")
+  #load("./data/contact_home.rda")
   #run the function with Great Britain and contact_home
   home_matrix <- get_contact_matrix("United Kingdom of Great Britain"
-                                    , contact_home, 21)
+                                    ,contact_home, 21)
 
   #expect the length of both dimensions of the output to be equal to 21.
   expect_length(home_matrix[1, ], 21)
