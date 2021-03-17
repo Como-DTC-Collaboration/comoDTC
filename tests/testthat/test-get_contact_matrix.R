1e61b213ee21985cfd7215bcba4c8179d5dfe475
Oscar Dimdore-Miles <32623897+oscardm20994@users.noreply.github.com>
2021-03-17 13:06:50 +0000

    added unit test for contact copying

--- tests/testthat/test-get_contact_matrix.R
+++ tests/testthat/test-get_contact_matrix.R
@@ -9,3 +9,23 @@ test_that("get_contact_matrix produces expected 21x21 square matrix output", {
   expect_length(home_matrix[1, ], 21)
   expect_length(home_matrix[, 1], 21)
 })
+
+test_that("get_contact_matrix copies contact entries for 75-80 years category
+          succesfully for age groups 80-100+", {
+
+  #run the function with Great Britain and contact_home
+  home_matrix <- get_contact_matrix("United Kingdom of Great Britain",
+                                    contact_home, 21)
+
+
+  #test whether the entries of output matrix corresponding to age categories 80+
+  # equal the contacts of the last age category
+
+  #loop over entries 16:21 of output matrix, test whether these are equal to
+  #entry 16.
+  for (i in 16:21) {
+  expect_equal(home_matrix[i, ], home_matrix[16, ])
+  expect_equal(home_matrix[, i], home_matrix[, 16])
+  }
+
+})
