7352adf8851fef4653bb940b8a48a39a8f00f74a
solveigvandervegt <sav57@cornell.edu>
2021-03-19 16:10:29 +0000

    Correct what rows and columns in contact matrices mean
    Sumali helpfully pointed out that I had it switched, and that in the data tables, contrary to in the heatmaps, the rows are individuals and that columns represent their contacts. This has now been corrected in the documentation.

--- R/data.R
+++ R/data.R
@@ -2,11 +2,12 @@
 #'
 #' A list of 152 16x16 asymmetrical contact matrices, one for each country,
 #' describing which age groups have contact with which other age groups AT HOME.
-#' The units of entries are the average number of contacts with a person in that
-#' age group in a day. The columns are the age groups 0-5 (X1), 5-10 (X2),
-#' 10-15 (X3), and continuing with 5 year gaps up to 80 years old.
-#' The rows are the age group of the contact, where row 1 is 0-5 year olds,
-#' row 2 is 5-10 year olds, etc. up to 80 year olds.
+#' The units of entries are the average number of contacts with a person in
+#' that age group in a day. The top row is individuals in the age group 0-5
+#' years, the second row is individuals in the age group 5-10 years, and so
+#' on torow 16 which is individuals aged 75-80. The columns represent the
+#' people they come into contact with, where column 1 is the age groups 0-5
+#' (X1), column to is the age group 5-10 (X2), and so to column 16 (ages 75-80).
 #'
 #' @format A Large list of 152 tibbles, each a 16x16 list of doubles.
 #'
@@ -18,10 +19,11 @@
 #' A list of 152 16x16 asymmetrical contact matrices, one for each country,
 #' describing which age groups have contact with which other age groups AT
 #' SCHOOL. The units of entries are the average number of contacts with a person
-#' in that age group in a day. The columns are the age groups 0-5 (X1),
-#' 5-10 (X2), 10-15 (X3), and continuing with 5 year gaps up to 80 years old.
-#' The rows are the age group of the contact, where row 1 is 0-5 year olds,
-#' row 2 is 5-10 year olds, etc. up to 80 year olds.
+#' in that age group in a day. The top row is individuals in the age group
+#' 0-5 years, the second row is individuals in the age group 5-10 years, and
+#' so on to row 16 which is individuals aged 75-80. The columns represent the
+#' people they come into contact with, where column 1 is the age groups 0-5
+#' (X1), column to is the age group 5-10 (X2), and so to column 16 (ages 75-80).
 #'
 #' @format A Large list of 152 tibbles, each a 16x16 list of doubles.
 #'
@@ -33,10 +35,11 @@
 #' A list of 152 16x16 asymmetrical contact matrices, one for each country,
 #' describing which age groups have contact with which other age groups AT WORK.
 #' The units of entries are the average number of contacts with a person in that
-#' age group in a day. The columns are the age groups 0-5 (X1), 5-10 (X2),
-#' 10-15 (X3), and continuing with 5 year gaps up to 80 years old.
-#' The rows are the age group of the contact, where row 1 is 0-5 year olds,
-#' row 2 is 5-10 year olds, etc. up to 80 year olds.
+#' age group in a day. The top row is individuals in the age group 0-5 years,
+#' the second row is individuals in the age group 5-10 years, and so on to
+#' row 16 which is individuals aged 75-80. The columns represent the people
+#' they come into contact with, where column 1 is the age groups 0-5 (X1),
+#' column to is the age group 5-10 (X2), and so to column 16 (ages 75-80).
 #'
 #' @format A Large list of 152 tibbles, each a 16x16 list of doubles.
 #'
@@ -48,11 +51,12 @@
 #' A list of 152 16x16 asymmetrical contact matrices, one for each country,
 #' describing which age groups have contact with which other age groups AT OTHER
 #' PLACES (not covered by the other contact matrices). The units of entries are
-#' the average number of contacts with a person in that age group in a day. The
-#' columns are the age groups 0-5 (X1), 5-10 (X2), 10-15 (X3), and continuing
-#' with 5 year gaps up to 80 years old. The rows are the age group of the
-#' contact, where row 1 is 0-5 year olds, row 2 is 5-10 year olds, etc. up to
-#' 80 year olds.
+#' the average number of contacts with a person in that age group in a day.
+#' The top row is individuals in the age group 0-5 years,
+#' the second row is individuals in the age group 5-10 years, and so on to
+#' row 16 which is individuals aged 75-80. The columns represent the people
+#' they come into contact with, where column 1 is the age groups 0-5 (X1),
+#' column to is the age group 5-10 (X2), and so to column 16 (ages 75-80).
 #'
 #' @format A Large list of 152 tibbles, each a 16x16 list of doubles.
 #'
