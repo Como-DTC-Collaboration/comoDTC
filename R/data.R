11b2f559aefbaa2b6ca29e2cda9cee855df8fd68
solveigvandervegt <sav57@cornell.edu>
2021-03-25 13:54:01 +0000

    Created function to make excel sheet and extended documentation
    Wrote a function (create_excel) that takes an excel template (Template_CoMo_CountryData_temp.xlsx), edits it to be country-specific and then saves it as a new excel file. The name of the new excel file is returned so the file can be processed as it is now.
    
    Documentation of the data sets cases, population, country_demog is added. mort_sever_data is standard across countries and thus already in the excel template. age_categories is not required, but I do not know how to delete mort_sever_data and age_categories from the data_CoMo.RData file.

--- R/data.R
+++ R/data.R
@@ -62,3 +62,33 @@
 #'
 #' @source \url{https://journals.plos.org/ploscompbiol/article?id=10.1371/journal.pcbi.1005697#sec020}
 "contact_other"
+
+#' cases
+#'
+#' A dataframe with 5 variables: date (Date), cases (num), deaths (num),
+#' country (chr) and cumulative_death (num).
+#' It records per country and per date, the number of COVID
+#' cases and the number of deaths, both on the day and cumulative.
+#'
+#' @format Dataframe with 5 columns, number of rows will differ per update.
+#'
+#' @source \url{https://www.ecdc.europa.eu/en/covid-19/data}
+
+#' population
+#'
+#' A dataframe with 5 variables: country (chr), age_category (Factor),
+#' pop (num), birth (num), death(num). It specifies per country and per age
+#' category, the size of the population, the number of births per person per
+#' day and the number of deaths per person per day. The age categories are
+#' 0-4 years, 5-9 years, 10-14 years, and so on, up to the last category,
+#' which is 100+ years old.
+#'
+#' @format Dataframe with 5 columns and 4221 rows.
+#'
+#' @source \url{https://population.un.org/wpp/Download/Standard/Population/}
+
+#' countries_demog
+#'
+#' A list of 201 countries (chr) that the model can take, in alphabetical order.
+#'
+#' @format list of chr
