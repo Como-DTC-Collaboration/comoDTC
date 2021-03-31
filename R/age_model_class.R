695792d1fb4bcd984df03028f90619ab8b7259d7
Ioana B <ioana.bouros@dtc.ox.ac.uk>
2021-03-31 23:39:59 +0100

    Improvements in methods of the class; need to fix RHS function

--- R/age_model_class.R
+++ R/age_model_class.R
@@ -1,4 +1,5 @@
 library(methods)
+library(deSolve)
 
 #define the age_model class.
 #Slots of this class are as follows:
@@ -9,11 +10,11 @@ library(methods)
 setClass("age_model",
          #slots
          slots = c(
-           name = "character",
-           output_names = "list",
-           parameter_names = "list",
+           name = 'character',
+           output_names = 'list',
+           parameter_names = 'list',
            parameters = 'list',
-           n_age_categories = "numeric"
+           n_age_categories = 'numeric'
          ),
 
          #prototypes for the slots, automatically set output and param
@@ -21,7 +22,8 @@ setClass("age_model",
          prototype = list(
            name = NA_character_,
            output_names = list('S', 'I', 'R', 'Incidence'),
-           parameter_names = list('S0', 'I0', 'R0', 'beta', 'kappa', 'gamma'),
+           parameter_names = list('S0', 'I0', 'R0', 'a', 'b'),
+           parameters = vector(mode = "list", length = 5),
            n_age_categories = NA_real_
 
          )
@@ -33,63 +35,67 @@ setClass("age_model",
 #N.B. this is not an S4 method, when I tried to use such a method I was unable
 #to assign slots of the object successfully.
 #function returns the age_model object with parameters slot assigned
-set_parameters <- function(object, S0, I0, R0, beta, kappa, gamma){
-  #create list of parameter values
-  params <- list(S0, I0, R0, beta, kappa, gamma)
-
-  #add names to each value
-  names(params) = object@parameter_names
-
-  #assign the params namelist to the object
-  object@parameters <- params
-
-  return(object)
-}
-
-
-
+setGeneric("get_parameters", function(object) standardGeneric("get_parameters"))
+setGeneric(
+  "set_parameters",
+  function(object, S0, I0, R0, a, b){
+    standardGeneric("set_parameters")
+  })
+setMethod("get_parameters", "age_model", function(object) object@parameters)
+setMethod(
+  "set_parameters", "age_model",
+  function(object, S0, I0, R0, a, b) {
+    #create list of parameter values
+    params <- list(S0, I0, R0, a, b)
+  
+    #add names to each value
+    names(params) = object@parameter_names
+  
+    #assign the params namelist to the object
+    object@parameters <- params
+    
+    return(object)
+  })
 
 #age_model class specific functions
-#Generic for function which gives RHS of odes
-setGeneric(name = "right_hand_side",
-           def = function(object, t, y, c){
-             standardGeneric("right_hand_side")
-           }
-)
-#set class specific method for RHS of odes
-setMethod(f = "right_hand_side", "age_model", function(object, t, y, c) {
-  #RHS code here
-})
-
-
 
 #Generic for function which solves system of odes
 setGeneric(name = "simulate",
-           def = function(object, parameters, times){
+           def = function(object, times){
              standardGeneric("simulate")
            }
 )
 #set class specific method to numerically solve odes
-setMethod(f = "simulate", "age_model", function(object, parameters,
-                                                       times) {
-  tstart =
-  tend =
-  #call RHS function to then use as input for the ode solver
-  RHS = right_hand_side(object, t, y, c)
-
-  #simulate code here output = ode(etc)
-
-  return(output)
-})
-
-
-
-
-
-
+setMethod(
+  "simulate", "age_model",
+  function(object, times) {
+    state <- c(S = get_parameters(object)$S0,
+               I = get_parameters(object)$I0,
+               R = get_parameters(object)$R0)
+    parameters <- c(a = get_parameters(object)$a,
+                    b = get_parameters(object)$b)
+    right_hand_side <- function(t, state, parameters) {
+      with(
+        as.list(c(state, parameters)),
+        {
+          # rate of change
+          dS <- -a*S*I
+          dI <- a*S*I - b*I
+          dR <- b*I
+          # return the rate of change
+          list(c(dS, dI, dR))
+        })
+    }
+    output <- ode(
+      y = state, times = times, func = right_hand_side, parms = parameters)
+    
+    return(output)
+  })
 
 #test case for creating an instance
-my_model <- new("age_model", name = "my first model", n_age_categories = 20)
+my_model <- new("age_model", name = "my_model", n_age_categories = 20)
 
-
+my_model <- set_parameters(my_model, c(1, 1), c(1, 0), c(0, 0), 1, 0.5)
+get_parameters(my_model)
+simulate(my_model, seq(0, 10, by = 1))
 
