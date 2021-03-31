8bf61798baac0395ad25f115711fcbd74446343d
Oscar Dimdore-Miles <32623897+oscardm20994@users.noreply.github.com>
2021-03-31 09:54:00 +0100

    changed class defs

--- R/age_model_class.R
+++ R/age_model_class.R
@@ -1,6 +1,7 @@
 library(methods)
 
-#define the age_model class
+#define the age_model class with slots for name of model and number
+#of age catergoreis
 setClass("age_model",
          #slots
          slots = c(
@@ -15,6 +16,24 @@ setClass("age_model",
          )
 )
 
+
+#method of the S4 age_model class that solves the system of age structured odes
+#
+
+#set generic function name for solving system of age structured odes
+setGeneric(name="solve_age_ode",
+           def=function(object)
+           {
+             standardGeneric("solve_age_ode")
+           }
+)
+#set class specific method
+setMethod(f = "solve_age_ode", "age_model", function(object) {
+
+  #ode solving method here
+  object@name <- 'blah'
+  return(object)
+})
 
 
 
