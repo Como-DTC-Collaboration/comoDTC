e5de4c2553455f35bbedf781a61f1379eef7b21e
hfldai <liangti.dai@linacre.ox.ac.uk>
2021-04-07 11:40:52 +0100

    add analytical solution to the current simple ode system

--- R/symptome_compartment_class.R
+++ R/symptome_compartment_class.R
@@ -39,7 +39,8 @@ setClass(Class = "model_symptome",
            name = "character",
            initial_population = 'list',
            parameters = 'list',
-           model_output = 'list'
+           model_output = 'list',
+           model_output_analytical = 'list'
          ),
          prototype = list(
            name = NA_character_,
@@ -47,7 +48,8 @@ setClass(Class = "model_symptome",
              setNames(list('S', 'E', 'I', 'HA', 'VentA', 'ICUA', 'R')),
            parameters = vector(mode='list', length=6) %>%
              setNames(list('lam', 'gamma', 'omega', 'ihr', 'phs', 'nu')),
-           model_output = data.frame()
+           model_output = data.frame(),
+           model_output_analytical = data.frame()
          ))
 
 #' Check validity of initail param and population settings
@@ -125,8 +127,8 @@ setGeneric(name = "ode_simulate",
            def = function(object, ...){standardGeneric("ode_simulate")}
            )
 
-#' param times: a list of time points
-#' param method: string, the ode integrator to use. Default is set to 'Isoda', same as the function ode().
+#' @param times A list of time points
+#' @param method String, the ode integrator to use. Default is set to 'Isoda', same as the function ode().
 #' 
 setMethod("ode_simulate", signature(object ='model_symptome'),
   function(object, times, method = 'lsoda') {
@@ -154,17 +156,19 @@ setMethod("ode_simulate", signature(object ='model_symptome'),
       with(
         as.list(c(pop_groups, params)),
         {
-          dSdt <- -lam*S - omega*R
+          dSdt <- -lam*S + omega*R
           dEdt <- lam*S - gamma*E 
           dIdt <- -nu$nui*I + (1.0-ihr)*gamma*E # - ratetestI*I
-          dRdt <-  -omega*R + nu$nui*I + nu$nuhsa*HA + nu$nu_icua*ICUA + nu$nu_venta*VentA
           dHAdt <- gamma*ihr*(1-(phs%>%unlist%>%sum))*E - nu$nuhsa*HA
           dICUAdt <- gamma*ihr*phs$phs_icua*E - nu$nu_icua*ICUA
           dVentAdt <- gamma*ihr*phs$phs_venta*E - nu$nu_venta*VentA
+          dRdt <-  -omega*R + nu$nui*I + nu$nuhsa*HA + nu$nu_icua*ICUA + nu$nu_venta*VentA
           
-          list(c(dSdt, dEdt, dIdt, dRdt, dHAdt, dICUAdt, dVentAdt))
+          list(c(dSdt, dEdt, dIdt, dHAdt, dICUAdt, dVentAdt, dRdt))
         })
     }
+    
+    # numeric solver
     output <- ode(
       y = pop_groups, 
       times = times, 
@@ -172,10 +176,9 @@ setMethod("ode_simulate", signature(object ='model_symptome'),
       parms = params,
       method = method)
     
-    
     output <- as.data.frame(output)
     
-    # normalize: Keep the output population as fraction (range(0,1)) for each time step
+    # normalize: keep the output population as fraction (range(0,1)) for each time step
     output.pop <- output[,-which(names(output) == "time")]
     output.pop <- sweep(output.pop, 1, apply(output.pop, 1, min))
     output[,-which(names(output) == "time")] <- output.pop/rowSums(output.pop)
@@ -184,22 +187,109 @@ setMethod("ode_simulate", signature(object ='model_symptome'),
   }
   )
 
+#'  ode_analytical to solve ode systems analytically (for the current simple system)
+setGeneric(name = "ode_analytical",
+           def = function(object, ...){standardGeneric("ode_analytical")}
+)
 
-# simple plot to observe population fraction change of each group by time
+#' @param times A list of time points
+#' 
+setMethod("ode_analytical", signature(object ='model_symptome'),
+          function(object, times) {
+            # initial size of each population group
+            pop_groups <- c(S = object@initial_population$S,
+                            E = object@initial_population$E,
+                            I = object@initial_population$I,
+                            HA = object@initial_population$HA,
+                            VentA = object@initial_population$VentA,
+                            ICUA = object@initial_population$ICUA,
+                            R = object@initial_population$R
+            )
+            
+            # parameters
+            params <- c(lam = object@parameters$lam,
+                        gamma = object@parameters$gamma,
+                        omega = object@parameters$omega,
+                        ihr = object@parameters$ihr,
+                        phs = object@parameters$phs,
+                        nu = object@parameters$nu 
+            )
+            
+            # build up linear system
+            linear_system <- function(pop_groups, params){
+              dimA = length(pop_groups)
+              with(
+                as.list(c(pop_groups, params)),
+                {
+                  A <- matrix(rep(0,dimA^2),nrow=dimA) #  S,E,I,HA,ICUA,VentA,R
+                  A[1, c(1,7)] = c(-lam, omega) # S 
+                  A[2, 1:2] = c(lam, -gamma) # E
+                  A[3, 2:3] = c((1.0-ihr)*gamma, -nu$nui)# I
+                  A[4, c(2,4)] = c(gamma*ihr*(1-(phs%>%unlist%>%sum)), -nu$nuhsa) # HA
+                  A[5, c(2,5)] = c(gamma*ihr*phs$phs_icua, -nu$nu_icua) # ICUA
+                  A[6, c(2,6)] = c(gamma*ihr*phs$phs_venta, -nu$nu_venta) # VentA
+                  A[7, 3:7] = c(nu$nui, nu$nuhsa, nu$nu_icua, nu$nu_venta, -omega)# R
+                  A
+                })
+            }
+            A <- linear_system(pop_groups = pop_groups, params = params)
+            
+            # analytical solution of ode system as a function of single time point t
+            ode_analytical_single_time <- function(t, A, initial){
+              A.eig <- eigen(A)
+              constants <- solve(A.eig$vectors,initial) 
+              ints <- matrix(rep(exp(t*A.eig$values), nrow(A)), nrow=nrow(A), byrow=T)
+              matrix((A.eig$vectors*ints) %*% constants)
+            }
+            output <- sapply(times, function(i)ode_analytical_single_time(i, A, as.numeric(pop_groups))) %>% 
+              t() %>%
+              as.data.frame()
+            
+            # normalize: keep the output population as fraction (range(0,1)) for each time step
+            output <- sweep(output, 1, apply(output, 1, min))
+            output <- output/rowSums(output)
+            colnames(output) <- names(pop_groups)
+            output$time <- times 
+            
+            output <- as.data.frame(output)
+            object@model_output_analytical <- output
+            return(object)
+
+                
+          }
+)
+
+
+#' simple plot to observe population fraction change of each group by time
+#' @param is_analytical Logical, indicates whether to plot the analytical or numeric ode outcome (default: FALSE).
 setGeneric(name = "plot_ode_output",
            def = function(object, ...){standardGeneric("plot_ode_output")}
 )
 setMethod("plot_ode_output", signature(object ='model_symptome'),
-          function(object) {
-            # reshape output frame 
-            output.melt <- reshape2::melt(object@model_output, id.vars="time")
-            names(output.melt) <- c("time","population_group", "fraction")
+          function(object, is_analytical=FALSE) {
+            if(is_analytical){
+              output <- object@model_output_analytical
+####### Chekc if the corr. solver ran
+            }else{
+              output <- object@model_output
+            }
+            if(empty(output)){
+              error <- paste0("Empty output. Please run the ode solver by calling ", 
+                              if(is_analytical) "ode_analytical" else "ode_simulate")
+              return (error)
+              
+            }else{
+              # reshape data frame 
+              output.melt <- reshape2::melt(output, id.vars="time")
+              names(output.melt) <- c("time","population_group", "fraction")
+              
+              # plot the population change of each group
+              p <- ggplot2::ggplot(data = output.melt, aes(x=time, y=fraction)) +
+                geom_line(aes(colour=population_group)) + 
+                theme_classic()
+              return (p)
+            }
             
-            # plot the population change of each group
-            p <- ggplot2::ggplot(data = output.melt, aes(x=time, y=fraction)) +
-              geom_line(aes(colour=population_group)) + 
-              theme_classic()
-            return (p)
           })
 
 
@@ -229,16 +319,23 @@ nu = list(nui=0.5,# I
           nu_venta=0.005, # VentA
           nu_icua=0.01) # ICUA
 
-# check_init(nes_model)
+
 nes_model <- new("model_symptome", name = "new_model")
+# check validity of parameter/input settings
 nes_model <- set_init(nes_model, 
                       S=S, E=E, I=I, HA=HA, VentA=VentA, ICUA=ICUA, R=R, # initial population size of each group
                       lam=lam, gamma=gamma, omega=omega, ihr=ihr, phs=phs, nu=nu) # parameters
 
 # simulation period: 100 time points
-nes_model <- ode_simulate(nes_model, seq(0, 100, by = 1), method="lsoda")
-# head(nes_model@model_output)
-# plot outcome
+t <- seq(0, 100, by = 0.1)
+# numerical solution and plot
+nes_model <- ode_simulate(nes_model, t, method="lsoda")
 plot_ode_output(nes_model)
+# analytical solution and plot
+nes_model <- ode_analytical(nes_model, t)
+plot_ode_output(nes_model, is_analytical=TRUE)
+
+
+
 
 
